ff15a596308f16c34b600a978164f5e69fbbe268
dorianreineccius <email@example.com>
2020-04-11 00:05:11 +0200

    Last working state

--- src/view/index.php
+++ src/view/index.php
@@ -7,59 +7,28 @@ require_once '../core/init.php';
     <title>PHPGallery</title>
 </head>
 <body>
-<div class="header">
-    <a class="active"
-       href="#">PHPGallery</a><?php echo HTMLBuilder::json_to_table(file_get_contents('../model/header.json')); ?>
-</div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div><p><? echo "hello world!" ?></p></div>
-<div class="footer"><?php echo HTMLBuilder::json_to_table(file_get_contents('../model/footer.json')); ?></a>
+<div class="page-container">
+    <div class="header">
+        <a class="active"
+           href="#">PHPGallery</a><?php echo HTMLBuilder::json_to_table(file_get_contents('../model/header.json')); ?>
+    </div>
+    <div class="content-wrap">
+        <div><p><? echo "1th line" ?></p></div>
+        <div><p><? echo "hello world!" ?></p></div>
+        <div><p><? echo "hello world!" ?></p></div>
+        <div><p><? echo "hello world!" ?></p></div>
+        <div><p><? echo "hello world!" ?></p></div>
+        <div><p><? echo "hello world!" ?></p></div>
+        <div><p><? echo "hello world!" ?></p></div>
+        <div><p><? echo "hello world!" ?></p></div>
+        <div><p><? echo "hello world!" ?></p></div>
+        <div><p><? echo "hello world!" ?></p></div>
+        <div><p><? echo "hello world!" ?></p></div>
+        <div><p><? echo "hello world!" ?></p></div>
+        <div><p><? echo "(n-1)th line" ?></p></div>
+        <div><p><? echo "(n)tn line" ?></p></div>
+    </div>
+    <div class="footer"><?php echo HTMLBuilder::json_to_table(file_get_contents('../model/footer.json')); ?></div>
 </div>
 </body>
 </html>
